ee38346a4d830b6619ff20a7581c61f47121bb8a
Anton Chernykh <antoshidza@mail.ru>
2025-01-23 17:40:41 +0300

    Implement suggestion selection and apply

--- Assets/Scripts/Console/ConsoleUiView.cs
+++ Assets/Scripts/Console/ConsoleUiView.cs
@@ -3,18 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.UIElements;
+// ReSharper disable PossibleMultipleEnumeration
 
 namespace Console
 {
     public class ConsoleUiView : MonoBehaviour
     {
+        private const int SuggestionsCount = 5;
+        private const string SuggestionSelectedClass = "suggestion-selected";
+        
         [SerializeField] private UIDocument _document;
         private ListView _logsList;
         private TextField _inputField;
-        private VisualElement _suggestions;
+        private VisualElement _suggestionsRoot;
         private readonly List<string> _logs = new();
         private readonly List<string> _submitted = new();
+        private readonly List<string> _suggestions = new();
         private int _restorePos;
+        private int _suggestionsPos;
 
         public IEnumerable<string> CommandNames; 
 
@@ -29,29 +35,33 @@ namespace Console
             _inputField.textSelection.selectionColor = Color.white;
             _inputField.RegisterValueChangedCallback(context =>
             {
+                _suggestionsRoot.Clear();
                 _suggestions.Clear();
-                _suggestions.visible = false;
+                _suggestionsPos = 0;
+                _suggestionsRoot.visible = false;
                 if (context.newValue == string.Empty) return;
                 var commands = CommandNames
                     .Where(command => command.Contains(context.newValue, StringComparison.OrdinalIgnoreCase))
                     .OrderBy(command => command.IndexOf(context.newValue, StringComparison.OrdinalIgnoreCase))
-                    .Take(5); 
+                    .Take(SuggestionsCount); 
                 if(!commands.Any() || commands.Contains(context.newValue)) return;
-                _suggestions.visible = true;
-                foreach (var command in commands.Take(5))
+                _suggestionsRoot.visible = true;
+                foreach (var command in commands.Take(SuggestionsCount))
                 {
                     var matchIndex = command.IndexOf(context.newValue, StringComparison.OrdinalIgnoreCase);
                     var label = new Label(command.Insert(matchIndex, "<b>").Insert(matchIndex + context.newValue.Length + 3, "</b>"));
                     label.AddToClassList("suggestion");
-                    _suggestions.Add(label);
+                    _suggestionsRoot.Add(label);
+                    _suggestions.Add(command);
                 }
+                _suggestionsRoot[0].AddToClassList(SuggestionSelectedClass);
             });
             
             _logsList.itemsSource = _logs;
             _logsList.bindItem = (root, itemIndex) => root.Q<Label>().text = _logs[itemIndex];
             _logsList.Q<ScrollView>().verticalScroller.value = _logsList.Q<ScrollView>().verticalScroller.highValue;
 
-            _suggestions = _document.rootVisualElement.Q("suggestions");
+            _suggestionsRoot = _document.rootVisualElement.Q("suggestions");
         }
 
         private void Update()
@@ -62,6 +72,8 @@ namespace Console
             if (Input.anyKey)
                 _inputField.Focus();
             
+            UpdateApplySuggestion();
+            UpdateSuggestionSelection();
             UpdateSubmitRestore();
         }
 
@@ -81,20 +93,48 @@ namespace Console
 
         private void UpdateSubmitRestore()
         {
-            if (_submitted.Count > 0)
+            if (_submitted.Count <= 0 || _suggestions.Count > 0) return;
+            if (Input.GetKeyDown(KeyCode.UpArrow))
             {
-                if (Input.GetKeyDown(KeyCode.UpArrow))
-                {
-                    _restorePos = (_restorePos + 1) % _submitted.Count;
-                    _inputField.value = _submitted[^(_restorePos + 1)];
-                }
-                else if (Input.GetKeyDown(KeyCode.DownArrow))
-                {
-                    _restorePos = (_restorePos - 1 + _submitted.Count) % _submitted.Count;
-                    _inputField.value = _submitted[^(_restorePos + 1)];
-                }
-                else if (Input.anyKeyDown) 
-                    _restorePos = 0;
+                _restorePos = (_restorePos + 1) % _submitted.Count;
+                _inputField.value = _submitted[^(_restorePos + 1)];
+            }
+            else if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                _restorePos = (_restorePos - 1 + _submitted.Count) % _submitted.Count;
+                _inputField.value = _submitted[^(_restorePos + 1)];
+            }
+            else if (Input.anyKeyDown) 
+                _restorePos = 0;
+        }
+
+        private void UpdateApplySuggestion()
+        {
+            if(!Input.GetKeyDown(KeyCode.Tab) || _suggestions.Count == 0) return;
+            _inputField.value = _suggestions[_suggestionsPos];
+        }
+
+        private void UpdateSuggestionSelection()
+        {
+            if(_suggestions.Count == 0) return;
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                _suggestionsPos = (_suggestionsPos + 1) % _suggestions.Count;
+                SelectSuggestion();
+            }
+            else if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                _suggestionsPos = (_suggestionsPos - 1 + _suggestions.Count) % _suggestions.Count;
+                SelectSuggestion();
+            }
+
+            return;
+
+            void SelectSuggestion()
+            {
+                foreach (var visualElement in _suggestionsRoot.Children())
+                    visualElement.RemoveFromClassList(SuggestionSelectedClass);
+                _suggestionsRoot[_suggestionsPos].AddToClassList(SuggestionSelectedClass);
             }
         }
 
